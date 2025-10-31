8e1e29963a61b121f2cd130b6db14f07804f8af8
chenxi <qichenxi@dreame.tech>
2025-10-31 11:27:24 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup></script>
 
 <template>
-  <div class="min-h-screen bg-gray-50 text-gray-800 p-6">
-    <RouterView />
-  </div>
+  <div class="min-h-screen bg-gray-50 text-gray-800 p-6"><RouterView />111</div>
 </template>
 
 <style scoped></style>
