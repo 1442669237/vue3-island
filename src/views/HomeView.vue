e890bfa5419926c25c45d6a4b2f88e4696b2c151
Alex <alexsunhandsome@gmail.com>
2025-10-31 13:31:33 +0800

    测试

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
     />
     <div class="p-4 bg-white rounded shadow-sm border border-gray-200">
       <button class="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700">
-        按钮示例
+        按钮示例1111
       </button>
     </div>
   </section>
